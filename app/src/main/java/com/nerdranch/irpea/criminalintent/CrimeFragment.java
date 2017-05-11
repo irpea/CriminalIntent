d1df08875b0258b2712f69de4f5b3f7a34b6912a
irpea <irpea@live.com>
2017-05-11 13:57:34 +0200

    Created fragment_crime.xml and CrimeFragment.java files

---
+++ app/src/main/java/com/nerdranch/irpea/criminalintent/CrimeFragment.java
@@ -0,0 +1,10 @@
+package com.nerdranch.irpea.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by irpea on 11.5.2017..
+ */
+
+public class CrimeFragment extends Fragment {
+}
