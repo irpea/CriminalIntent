6fce99bd64b27514b7137b34a7a172c26a88e18b
irpea <irpea@live.com>
2017-05-11 13:37:04 +0200

    Adding Id, Title, Date, solved boolean and a constructor to Crime class

---
+++ app/src/main/java/com/nerdranch/irpea/criminalintent/Crime.java
@@ -0,0 +1,18 @@
+package com.nerdranch.irpea.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+}
