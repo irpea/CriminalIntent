aa3ed44b7921fbf107251d41ea5ac8ff88468c0f
irpea <irpea@live.com>
2017-05-14 04:34:19 +0200

    Implemented SingleFragment Activity to CrimeActivity Made CrimeListFragment with SignleFragmnetActivityon CrimeListActivity

--- app/src/main/java/com/nerdranch/irpea/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/nerdranch/irpea/criminalintent/CrimeListActivity.java
@@ -1,13 +1,14 @@
 package com.nerdranch.irpea.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeListActivity extends AppCompatActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
     }
 }
