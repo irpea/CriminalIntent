2d3ff171351ba53ddeb1bec58499f3573e931fb0
irpea <irpea@live.com>
2017-06-25 16:42:42 +0200

    I made simple AlertDialog in a class named DatePickerFragment.java with a title and a positive button and connected it to a CrimeFragments Date Button.
    Started creating new dialog_date.xml file witf DatePicker as a root element.

--- app/src/main/java/com/nerdranch/irpea/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/nerdranch/irpea/criminalintent/CrimeFragment.java
@@ -3,6 +3,7 @@ package com.nerdranch.irpea.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -21,6 +22,7 @@ import static android.widget.CompoundButton.*;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -70,7 +72,14 @@ public class CrimeFragment extends Fragment {
             }
         });
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
         mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
