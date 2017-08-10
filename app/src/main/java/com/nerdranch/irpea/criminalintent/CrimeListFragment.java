a84d6f79451477cdc69cc4c650446224da56de07
irpea <irpea@live.com>
2017-08-10 13:16:55 +0200

    Done with removing java database and making of SQL database. Finished Ch. 14 of bnrg.

--- app/src/main/java/com/nerdranch/irpea/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/nerdranch/irpea/criminalintent/CrimeListFragment.java
@@ -87,6 +87,10 @@ public class CrimeListFragment extends Fragment {
         public int getItemCount() {
             return mCrimes.size();
         }
+
+        public void setCrimes(List<Crime> crimes){
+            mCrimes = crimes;
+        }
     }
 
     @Nullable
@@ -123,6 +127,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
         updateSubtitle();
